d8ad58d5a22f0e2d8dffd41db2376ea14f10a83a
laythex <kalashnikov.mm@phystech.edu>
2024-12-07 14:51:09 +0300

    541

---
+++ py/541.py
@@ -0,0 +1,166 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import regressions as reg
+from kurwa_fit import kurwa_fit
+
+pn = 760
+Tn = 15 + 273.15
+T = 25 + 273.15
+rho0 = 101325 * 29 * 1e-3 / 8.31 / Tn * 1e-3 # г/см^3
+
+P1 = np.array([730, 700, 675, 650, 625, 600, 575, 550, 525, 500,
+               475, 450, 425, 400, 375, 350, 325, 300, 275, 250,
+               225, 200, 175, 150, 125, 100, 75, 50, 25, 0])
+
+P0 = 750.5
+P = P0 - P1
+sP = np.sqrt(0.8 ** 2 + 0.5 ** 2)
+
+N = np.array([3597, 3459, 3159, 2877, 2614, 2285, 1862, 1353, 831, 370,
+              78, 1, 1, 3, 3, 2, 2, 1, 2, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+n = N / 10
+sn = np.sqrt(N) / 10
+
+I = np.array([19, 63, 97, 134, 168, 209, 248, 289, 329, 367,
+              409, 454, 496, 547, 590, 640, 691, 735, 783, 833,
+              870, 893, 900, 899, 895, 890, 885, 875, 870, 865])
+
+sI = 1
+
+d = np.array([0, 1, 2, 3, 4, 5, 6, 6.5, 6.75,
+              7, 7.25, 7.5, 7.75, 8, 8.25, 8.5, 8.75,
+              9, 9.25, 9.5, 9.75, 10, 10.25, 10.5]) + 10
+
+t = np.array([59.892, 59.700, 60.075, 60.049, 60.104, 59.975, 60.168, 59.896,
+              60.202, 60.038, 60.102, 59.608, 60.278, 60.115, 59.945, 60.440,
+              65.841, 81.176, 61.018, 60.087, 60.013, 60.341, 59.931, 60.056])
+
+M = np.array([847, 1002, 981, 827, 880, 856, 849, 809,
+              752, 683, 649, 540, 434, 367, 235, 104,
+              72, 56, 27, 16, 16, 12, 22, 12])
+
+m = M / t
+sm = np.sqrt(M) / t
+
+size = 10
+
+m1, m2 = 7, 16
+
+plt.scatter(d, m, s=size, color='black')
+plt.errorbar(d, m, yerr=sm, fmt='none', color='black')
+cse, cove = kurwa_fit(reg.lin, d[m1:m2], m[m1:m2], sigma=sm[m1:m2])
+k, b = cse
+x1e = float(-b / k)
+sx1e = float(1 / k ** 2 * np.sqrt(k ** 2 * cove[1][1] + b ** 2 * cove[0][0] - 2 * k * b * cove[0][1]))
+reg.plot_func(reg.lin, np.append(d[m1:m2], x1e), cse)
+x1m = float((d[m1] + d[m2]) / 2)
+sx1m = float(0.25 / np.sqrt(2))
+
+rho = rho0 * (P0 / pn) * (Tn / T) / 10 * 1e3
+plt.axvline(x1e, ls='--', color='black')
+plt.axvline(x1m, ls='--', color='black')
+print(f'x1e = {x1e * rho, sx1e * rho}')
+print(f'x1m = {x1m * rho, sx1m * rho}')
+
+plt.xlabel(f'x, мм')
+plt.ylabel(f'N, 1/с')
+plt.grid()
+
+plt.savefig(f'../images/541-4.png', dpi=300)
+# plt.show()
+plt.cla()
+
+n_max = 14
+n1, n2 = 5, 10
+n0 = 7
+
+plt.scatter(P[:n_max], n[:n_max], s=size, color='black')
+plt.errorbar(P[:n_max], n[:n_max], xerr=sP, yerr=sn[:n_max], fmt='none', color='black')
+cse, cove = kurwa_fit(reg.lin, P[n1:n2], n[n1:n2], sigma=sn[n1:n2])
+k, b = cse
+p2e = float(-b / k)
+sp2e = float(1 / k ** 2 * np.sqrt(k ** 2 * cove[1][1] + b ** 2 * cove[0][0] - 2 * k * b * cove[0][1]))
+reg.plot_func(reg.lin, np.append(P[n1:n2], p2e), cse)
+
+ns = [0]
+sns = [0]
+for i in range(1, n_max - 1):
+    na = n[i + 1] - n[i - 1]
+    Pa = P[i + 1] - P[i - 1]
+    ns.append(na / Pa)
+    sna = sn[i + 1] ** 2 + sn[i - 1] ** 2
+    spa = sP ** 2 + sP ** 2
+    sns.append(np.sqrt(sna / na ** 2 + spa / Pa ** 2))
+ns.append(0)
+sns.append(0)
+
+csm, covm = kurwa_fit(reg.parabola_centered,
+                      P[n0 - 2:n0 + 3], ns[n0 - 2:n0 + 3], sigma=sns[n0 - 2:n0 + 3])
+p2m = float(csm[0])
+sp2m = float(np.sqrt(covm[0][0]))
+
+plt.axvline(p2e, ls='--', color='black')
+plt.axvline(p2m, ls='--', color='black')
+
+x0 = 90
+x2m = x0 * p2m / pn * Tn / T
+x2e = x0 * p2e / pn * Tn / T
+sx2m = x0 * sp2m / pn * Tn / T
+sx2e = x0 * sp2e / pn * Tn / T
+
+rhom = rho0 * (p2m / pn) * (Tn / T) / 10 * 1e3
+rhoe = rho0 * (p2e / pn) * (Tn / T) / 10 * 1e3
+print(f'x2e = {x2e * rhoe, sx2e * rhoe}')
+print(f'x2m = {x2m * rhom, sx2m * rhom}')
+
+plt.xlabel(f'P, мм рт. ст.')
+plt.ylabel(f'N, 1/с')
+plt.grid()
+
+plt.savefig(f'../images/541-5.png', dpi=300)
+plt.show()
+plt.cla()
+
+plt.scatter(P[1:n_max - 2], ns[1:n_max - 2], s=size, color='black')
+plt.errorbar(P[1:n_max - 2], ns[1:n_max - 2], xerr=sP, yerr=sns[1:n_max - 2], fmt='none', color='black')
+reg.plot_func(reg.parabola_centered, P[n0 - 2:n0 + 3], csm)
+
+plt.axvline(p2m, ls='--', color='black')
+
+plt.xlabel('P, мм рт. ст.')
+plt.ylabel(r'dN/dP, 1/(с $\cdot$ мм рт. ст.)')
+plt.grid()
+
+plt.savefig(f'../images/541-6.png', dpi=300)
+plt.show()
+plt.cla()
+
+i1, i2 = 6, 20
+j1, j2 = 23, 30
+plt.scatter(P, I, s=size, color='black')
+plt.errorbar(P, I, xerr=sP, yerr=sI, fmt='none', color='black')
+cse1, cove1 = kurwa_fit(reg.lin, P[i1:i2], I[i1:i2], sigma=sI)
+cse2, cove2 = kurwa_fit(reg.lin, P[j1:j2], I[j1:j2], sigma=sI)
+reg.plot_func(reg.lin, P[i1:i2 + 3], cse1)
+reg.plot_func(reg.lin, P[j1 - 3:j2], cse2)
+k1, b1 = cse1
+k2, b2 = cse2
+p3e = float((b2 - b1) / (k1 - k2))
+sp3e = float(1 / (k1 - k2) ** 2 * np.sqrt(cove1[1][1] + cove2[1][1] +
+                                    p3e ** 2 * (cove1[0][0] + cove2[0][0]) +
+                                    2 * p3e * (cove1[0][1] + cove2[0][1])))
+plt.axvline(p3e, ls='--', color='black')
+
+R = 50
+x3e = R * p3e / pn * Tn / T
+sx3e = R * sp3e / pn * Tn / T
+print(f'x3e = {x3e, sx3e}')
+
+plt.xlabel('P, мм рт. ст.')
+plt.ylabel('I, пА')
+plt.grid()
+
+# plt.show()
+plt.cla()
